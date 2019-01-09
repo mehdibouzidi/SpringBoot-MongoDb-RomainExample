5cfc4eafb11859f6ef1a6211c67da6dcc15515ad
BOUZIDI Mehdi <mehdi.bouzidi.92@gmail.com>
2019-01-09 10:36:48 +0100

    INITIAL

---
+++ src/main/java/learn/springbmongodb/romain/springbmongodbromain/SpringbmongodbromainApplication.java
@@ -0,0 +1,14 @@
+package learn.springbmongodb.romain.springbmongodbromain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbmongodbromainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbmongodbromainApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/learn/springbmongodb/romain/springbmongodbromain/SpringbmongodbromainApplicationTests.java
@@ -0,0 +1,17 @@
+package learn.springbmongodb.romain.springbmongodbromain;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbmongodbromainApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
